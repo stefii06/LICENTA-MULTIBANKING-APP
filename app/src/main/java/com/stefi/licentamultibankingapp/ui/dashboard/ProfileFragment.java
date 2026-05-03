380dde053ca82dc044d18c6983e2322f44686e9a
Stefi <stefi@gmail.com>
2026-05-04 00:02:21 +0300

    Dashboard: adaugare fragmente placeholder pentru cele 4 tab-uri secundare

---
+++ app/src/main/java/com/stefi/licentamultibankingapp/ui/dashboard/ProfileFragment.java
@@ -0,0 +1,21 @@
+package com.stefi.licentamultibankingapp.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.stefi.licentamultibankingapp.R;
+
+public class ProfileFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
